1e15319e757e9939f41f3ddafc790d124068d88e
da895 <36041044+da895@users.noreply.github.com>
2020-12-21 15:23:17 +0800

    Update run.bat

--- batch/run.bat
+++ batch/run.bat
@@ -4,7 +4,7 @@ SETLOCAL ENABLEEXTENSIONS
 
 :: Check interactive
 SET noninteractive=1
-echo¡¡%CMDCMDLINE% | FINDSTR /L /B %COMSPEC% >NUL 2>&1
+echoÂ¡Â¡%CMDCMDLINE% | FINDSTR /L /B %COMSPEC% >NUL 2>&1
 IF %ERRORLEVEL% == 0 SET noninteractive=0
 
 SET me=%~n0
@@ -12,6 +12,13 @@ SET parent=%~dp0
 
 SET log=%TEMP%\%me%.txt
 IF EXIST %log% DEL /Q %log% >NUL
+
+for /f "delims=" %%a in ('vmic OS Get localdatetime ^| find "."') do SET DateTime=%%a
+
+SET filename=name_%DateTime:~0,8%.txt
+
+SET Today=%DATE%
+SET now=%TIME%
 
 :: The "main" logic of the script
 ::
